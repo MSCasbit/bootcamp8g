92c8fbd91c3a7a16f446dac5c5b30aaccc18d3c8
mauricio <castillo_smauricio292@outlook.com>
2020-05-26 03:04:08 -0500

    :building_construction: [INIT] practica JS OA

---
+++ s7JavaScript/js/main.js
@@ -0,0 +1,11 @@
+var a= 3
+var b= 7 
+var c= 8
+var resultAdd= a+b+c
+var resultSub= a-b-c
+var resultMod= b%a%c
+var resultMul= a*b*c
+console.log(resultAdd)
+console.log(resultSub)
+console.log(resultMod)
+console.log(resultMul)
