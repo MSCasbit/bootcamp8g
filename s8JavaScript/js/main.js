232fa58a35fa7f0c56e02c4055cf4bc9fecbe5c2
mauricio <castillo_smauricio292@outlook.com>
2020-05-27 14:50:22 -0500

    :sparkles: [INIT] uso de condiciones

---
+++ s8JavaScript/js/main.js
@@ -0,0 +1,28 @@
+var number= 2
+if(number%2==0){
+    console.log("The Number Is Pair")
+}else{
+    console.log("The Number Isn´t Pair")
+}
+var sides=4
+ if(sides===1){
+     console.log("It´s a Circle")
+ }else if(sides===3){
+     console.log("It´s a Triangle")
+ }else if(sides===4){
+     console.log("It´s a Square")
+ }else if(sides===5){
+     console.log("It´s a penta")
+ }else if(sides>=6){
+     console.log("This figure has a lot sides")
+ }
+
+ var base
+ var height
+  if(base===height){
+      console.log("It´s a Square")
+  }else if(base!==height){
+      console.log("It´s a Rectangle")
+  }else{
+
+  }
